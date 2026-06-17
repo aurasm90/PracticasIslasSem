ab02b31a5dc8763c9c1de23d0285ddc595808d9a
aurasm90 <111882449+aurasm90@users.noreply.github.com>
2026-06-17 14:13:07 +0200

    Inicio webscrapping

--- scraping.py
+++ scraping.py
@@ -1,1 +1,44 @@
-# Aura: abre el navegador automáticamente, navega por la web de contratación, busca las licitaciones y extrae los datos.
+# Aura:
+
+# PASOS:
+# 1 - Entrar en la web
+# 2 - Filtrar por Canarias
+# 3 - Leer TODAS las páginas de resultados
+# 4 - Para cada órgano con licitaciones > 0 entrar y extraer las licitaciones
+# 5 - Comparar con las ya vistas en JSON
+# 6 - Si hay nuevas → ENVIAR por email
+# 7 - Guardar las nuevas en JSON
+
+
+# ------------------------------
+# PASO 0 - Importamos librerías necesarias para Webscrapping
+# ------------------------------
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import json
+import os
+import time
+
+# ------------------------------
+# PASO 1 - Iniciar Navegador
+# ------------------------------
+
+def iniciar_navegador():
+    options = webdriver.ChromeOptions()
+    # Comenta esta línea mientras desarrollas para VER lo que hace
+    # options.add_argument("--headless")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+
+# ------------------------------
+# PASO 2 - Obtener lista de Órganos de Canarias
+# ------------------------------
